beac17e50d6113db2e46e61728f4ecee7ac1f6e7
William Bernoudy <william@williambernoudy.com>
2016-12-10 16:45:24 -0800

    Finished UTM, working on functions for converting machine to SD

--- utm.rkt
+++ utm.rkt
@@ -2,12 +2,16 @@
 (define-namespace-anchor a)
 (define ns (namespace-anchor->namespace a))
 
+(require "TM.rkt")
+
 (struct sk (mconfig_to_call mtable) #:transparent)
 (struct instr (mconfig read_symbol ops final_mconfig) #:transparent)
+(define (instr->list i)
+  (list (instr-mconfig i) (instr-read_symbol i) (instr-ops i) (instr-final_mconfig i)))
 
 (define (printsk s)
   (displayln (sk-mconfig_to_call s))
-  (for-each displayln (sk-mtable s)))
+  (for-each displayln (set->list (sk-mtable s))))
 
 ;; builds the new m-config name from a list of symbols or sk's (as nesting
 ;; skeleton tables in one instruction line will thus return sk type
@@ -22,19 +26,19 @@
 
 
 (define (expand_table mconfigs)
-  (cond [(null? mconfigs) '()]
+  (cond [(null? mconfigs) (set)]
         [(list? (car mconfigs))
-           (append (expand_table (car mconfigs))
-                   (expand_table (cdr mconfigs)))]
+           (set-union (expand_table (car mconfigs))
+                      (expand_table (cdr mconfigs)))]
         [(symbol? (instr-final_mconfig (car mconfigs)))
-           (cons (car mconfigs) (expand_table (cdr mconfigs)))]
+           (set-add (expand_table (cdr mconfigs)) (car mconfigs))]
         ;; if the final_mconfig of the instr is itself a skeleton table,
         ;; we need to expand it and ensure that its table gets properly appended
         [(sk? (instr-final_mconfig (car mconfigs)))
-           (cons (struct-copy instr (car mconfigs) 
-                              [final_mconfig (sk-mconfig_to_call (instr-final_mconfig (car mconfigs)))])
-                 (append (expand_table (sk-mtable (instr-final_mconfig (car mconfigs))))
-                         (expand_table (cdr mconfigs))))]
+           (set-add (set-union (expand_table (set->list (sk-mtable (instr-final_mconfig (car mconfigs)))))
+                               (expand_table (cdr mconfigs)))
+                    (struct-copy instr (car mconfigs) 
+                                 [final_mconfig (sk-mconfig_to_call (instr-final_mconfig (car mconfigs)))]))]
         [else (raise (list "expand_table encountered a poorly formed instr in mconfigs" (car mconfigs)))]))
 
 (define (append_symbols . symbols)
@@ -84,12 +88,12 @@
                 (instr this_mconfig '!@ 'L this_mconfig)
 
                 (instr (m 1) alpha 'N C)
-                (instr (m 1) (s+ '! alpha) 'R (m 1))
                 (instr (m 1) 'None 'R (m 2))
+                (instr (m 1) (tnot alpha) 'R (m 1))
 
                 (instr (m 2) alpha 'N C)
-                (instr (m 2) (tnot alpha) 'R (m 1))
-                (instr (m 2) 'None 'R B))))))
+                (instr (m 2) 'None 'R B)
+                (instr (m 2) (tnot alpha) 'R (m 1)))))))
 
 ;; e(C, B, alpha)
 (define (erase C B alpha)
@@ -151,11 +155,10 @@
           (list (instr this_mconfig 'Else 'N (find (right C) B alpha)))))))
 
 ;; instr_pattern: '(instr this_mconfig beta 'N (find B C beta))
-(define (sub_symbol symbols_left instr_pattern)
+(define (sub_symbol symbols_left placeholder instr_pattern)
   (cond [(null? symbols_left) '()]
         [else
-          (letrec ([placeholder (caddr instr_pattern)]
-                   [sub (lambda (p sym)
+          (letrec ([sub (lambda (p sym)
                           (cond [(null? p) '()]
                                 [(equal? placeholder (car p))
                                   (cons sym (sub (cdr p) sym))]
@@ -164,10 +167,10 @@
                                 [else
                                   (cons (car p) (sub (cdr p) sym))]))])
                   (cons (eval (sub instr_pattern (car symbols_left)) ns)
-                        (sub_symbol (cdr symbols_left) instr_pattern)))]))
+                        (sub_symbol (cdr symbols_left) placeholder instr_pattern)))]))
 
-(define symbol_list '(0 1 'A 'C 'D))
-(define negated_symbol_list '('!0 '!1 '!A '!C '!D))
+(define symbol_list '('\0 '\1 'A 'C 'D 'L 'R 'N))
+(define negated_symbol_list '('!0 '!1 '!A '!C '!D '!L '!R '!N))
 (define match_sugar ((curry sub_symbol) symbol_list))
 (define not_match_sugar ((curry sub_symbol) negated_symbol_list))
 
@@ -181,7 +184,7 @@
     (sk this_mconfig
         (expand_table
           (list (instr this_mconfig 'Else 'N (find_left (m 1) B alpha))
-                (match_sugar
+                (match_sugar 'beta
                   `(instr ',(m 1) beta 'N (print_end ',C beta))))))))
 
 ;; ce(C, B, alpha)
@@ -242,12 +245,12 @@
     (sk this_mconfig
         (expand_table
           (list (instr this_mconfig 'Else 'N (find_left (m 1) (find A E beta) alpha))
-                (match_sugar
+                (match_sugar 'gamma
                   `(instr ',(m 1) gamma 'N (find_left (s+ ',(m 2) '-> gamma) ',A ',beta)))
-                (match_sugar
-                  `(instr ',(m 2) gamma 'N ',C))
-                (not_match_sugar
-                  `(instr ',(m 2) gamma 'N ',A)))))))
+                (match_sugar 'gamma
+                  `(instr (s+ ',(m 2) '-> gamma) gamma 'N ',C))
+                (match_sugar 'gamma
+                  `(instr (s+ ',(m 2) '-> gamma) (s+ '! gamma) 'N ',A)))))))
 
 ;; cpe(C. A. E. alpha, beta)
 (define (compare_and_erase C A E alpha beta)
@@ -339,8 +342,11 @@
         (expand_table
           (list (instr this_mconfig '!A '(R R) this_mconfig)
                 (instr this_mconfig 'A (list 'L (s+ 'P alpha) 'R) (m 1))
+
                 (instr (m 1) 'A (list 'R (s+ 'P alpha) 'R) (m 1))
                 (instr (m 1) 'D (list 'R (s+ 'P alpha) 'R) (m 2))
+                (instr (m 1) 'None (list 'PD 'R (s+ 'P alpha) 'R 'R 'R) C)
+
                 (instr (m 2) 'C (list 'R (s+ 'P alpha) 'R) (m 2))
                 (instr (m 2) '!C '(R R) C))))))
 
@@ -384,13 +390,13 @@
               (instr 'mk3 '!: '(R Pv L L L) 'mk3)
               (instr 'mk3 ': 'N 'mk4)
 
-              (instr 'mk4 'Else 'None (con (left (left 'mk5)) '_))
+              (instr 'mk4 'Else 'N (con (left (left 'mk5)) '_))
 
               (instr 'mk5 'Any '(R Pw R) 'mk5)
               (instr 'mk5 'None 'P: 'sh))))
 
 (define sh (expand_table
-        (list (instr 'sh 'Else 'None (find 'sh1 'inst 'u))
+        (list (instr 'sh 'Else 'N (find 'sh1 'inst 'u))
               (instr 'sh1 'Else '(L L L) 'sh2)
 
               (instr 'sh2 'D '(R R R R) 'sh3)
@@ -400,27 +406,108 @@
               (instr 'sh3 '!C 'N 'inst)
 
               (instr 'sh4 'C '(R R) 'sh5)
-              (instr 'sh4 '!C 'N (print_end2 'inst 0 ':))
+              (instr 'sh4 '!C 'N (print_end2 'inst '\0 ':))
 
               (instr 'sh5 'C 'N 'inst)
-              (instr 'sh5 '!C 'N 'inst))))
+              (instr 'sh5 '!C 'N (print_end2 'inst '\1 ':)))))
+
+;;(length (sk-mtable (copy 'c 'b 'x)))
+;;(length (sk-mtable (copy_and_erase 'c 'b 'x)))
+;;(printsk (copy_and_erase 'c 'b 'x))
+;;(printsk (copy_and_erase_all 'ov 'w))
 
 (define inst (expand_table
     (list (instr 'inst 'Else 'N (find_last (left 'inst1) 'u))
-          (match_sugar
+          (match_sugar 'alpha
             `(instr 'inst1 alpha '(R P_) (s+ 'inst1< alpha '>)))
-          ;;(instr 'inst<L> 'Else 'N (copy_and_erase_n 'ov 'v 'y 'x 'u 'w))
-          (instr 'inst<L> 'Else 'N (copy_and_erase_all 
-                                     (copy_and_erase_all
-                                       (copy_and_erase_all
-                                         (copy_and_erase_all
-                                           (copy_and_erase_all 'ov 'w)
-                                           'u)
-                                         'x)
-                                       'y)
-                                     'v))
-          ;;(instr 'inst<R> 'Else 'N (copy_and_erase_n 'ov 'v 'x 'u 'y 'w))
-          ;;(instr 'inst<N> 'Else 'N (copy_and_erase_n 'ov 'v 'x 'y 'u 'w))
+          (instr 'inst1<L> 'Else 'N (copy_and_erase_n 'ov 'v 'y 'x 'u 'w))
+          (instr 'inst1<R> 'Else 'N (copy_and_erase_n 'ov 'v 'x 'u 'y 'w))
+          (instr 'inst1<N> 'Else 'N (copy_and_erase_n 'ov 'v 'x 'y 'u 'w))
           (instr 'ov 'Else 'N (erase_all_markers 'anf)))))
 
-(length (append kom begin anf kmp sim mk sh inst))
+
+(define (instr_set->list instr_set)
+  (cond [(set-empty? instr_set) '()]
+        [(eq? (instr-read_symbol (set-first instr_set)) 'Else)
+           (append (list
+                      (instr->list (struct-copy instr (set-first instr_set)
+                                                [read_symbol 'Any]))
+                      (instr->list (struct-copy instr (set-first instr_set)
+                                                [read_symbol 'None])))
+                   (instr_set->list (set-rest instr_set)))]
+        [else
+          (cons (instr->list (set-first instr_set))
+                (instr_set->list (set-rest instr_set)))]))
+
+(define (sort_instr_list instr_list)
+  (sort instr_list
+        (lambda (x y)
+          (cond [(eq? (car x) (car y))
+                   (cond [(eq? (cadr x) 'None) #t]
+                         [(eq? (cadr y) 'None) #f]
+                         [(eq? (cadr x) 'Any) #f]
+                         [(eq? (cadr y) 'Any) #t]
+                         [else (symbol<? (cadr x) (cadr y))])]
+                [else
+                  (symbol<? (car x) (car y))]))))
+
+(define utm (sort_instr_list (instr_set->list (set-union kom begin anf kmp sim mk sh inst))))
+
+;;(for-each displayln utm)
+
+(define directions (set 'L 'R 'N))
+
+(define (stripP s)
+  (string->symbol (string-trim (symbol->string s) "P" #:right? #f)))
+
+(define (single_to_double_op op)
+  (cond [(set-member? directions op) (list 'None op)]
+        [else (list (stripP op) 'N)]))
+
+(define (convert_to_double_op i)
+  (let ([ops (caddr i)])
+       (cond [(null? ops) '()]
+             [(symbol? ops) (list i)]
+             [(= (length ops) 1) (list-set i 2 (single_to_double_op (car ops)))]
+             [else
+               (let ([new_mconfig (gensym)])
+                   (cond [(and (not (set-member? directions (car ops))) (set-member? directions (cadr ops)))
+                            (cons
+                              (append (take i 2) (list (list (stripP (car ops)) (cadr ops)) 
+                                                             (if (= (length ops) 2) 
+                                                                 (cadddr i)
+                                                                 new_mconfig)))
+                              (convert_to_double_op (list new_mconfig 'Any (cddr ops) (cadddr i))))]
+                         [else
+                           (cons
+                             (append (take i 2) (list (single_to_double_op (car ops)) new_mconfig))
+                             (convert_to_double_op (list new_mconfig 'Any (cdr ops) (cadddr i))))]))])))
+
+
+(convert_to_double_op '(wut a (R R R Pg L Pe R) wut2))
+
+(define (expand_ops instr_list)
+  (cond [(null? instr_list) '()]
+        [else
+          (append (convert_to_double_op (car instr_list))
+                  (expand_ops (cdr instr_list)))]))
+
+
+(expand_ops '((wut a (R R R Pg L Pe R) wut2)
+              (wut2 Any (R Px R Py R R Pz) wut)))
+
+#|
+(define (fix_print instr_list)
+  (map (lambda (i)
+         (cond [(and (eq? (cadr i) 'Any) (eq? (car (caddr i)) 'None))
+                             
+                |#
+                    
+       
+
+#|
+(machine '(@@^_D_A_D_D_C_C_L_D_A_$
+            0 begin)
+         '(100000 0 1)
+         utm)
+|#
